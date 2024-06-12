0ef3cebf308a1f5e70554f33afb78260802907c6
Nicolai-Bak <nicolai.bak@gmail.com>
2024-06-12 16:08:42 +1000

    setting some vim options

--- init.lua
+++ init.lua
@@ -1,6 +1,46 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
+vim.g.have_nerd_font = true
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.mouse = "a" -- enabled for all modes
+
+vim.opt.clipboard = "unnamedplus" -- syncs clipboard inside neovim with clipboard of OS
+
+vim.opt.breakindent = true -- keeps indentation of broken lines
+
+vim.opt.ignorecase = true -- default not casesensitive
+vim.opt.smartcase = true -- but become when capital letters is used
+
+-- Decrease mapped sequence wait time
+-- Displays which-key popup sooner
+vim.opt.timeoutlen = 200
+
+vim.opt.signcolumn = "yes" -- always make room in left margin for signs
+
+vim.opt.splitright = true -- open vsplit to the right
+vim.opt.splitbelow = true -- open split below
+
+-- TODO: consider if list and listchars is wanted
+-- vim.opt.list = true
+-- vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+
+vim.opt.inccommand = "split" -- preview during substitution
+
+vim.opt.cursorline = true -- highlight active cursor line
+
+vim.opt.scrolloff = 5 -- keep this number of lines above or below curser when scrolling
+
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>") -- clear search highlight on <Esc> but does not clear search
+
+-- The m marks the current position, and the backticks (```) are used to jump back to this mark
+-- Keymaps requires low timeoutlen to be useful
+vim.keymap.set("n", "oo", "m`o<Esc>``", { desc = "Insert line below" })
+vim.keymap.set("n", "OO", "m`O<Esc>``", { desc = "Insert line above" })
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
 	vim.fn.system({
